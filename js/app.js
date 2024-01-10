2176fd04d4e53bf9137a30ef7f3155031707753c
Meric Gulen <mericgulen@hotmail.com>
2024-01-10 15:03:40 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+//* show/hide menu
+
+const navMenu = document.querySelector('.header__nav');
+const openMenu = document.querySelector('.open');
+const closeMenu = document.querySelector('.close');
+
+openMenu.addEventListener('click', () => {
+  navMenu.style.visibility = 'visible';
+  navMenu.style.right = '0';
+  openMenu.style.display = 'none';
+   closeMenu.style.display = 'inline-block';
+});
+
+closeMenu.addEventListener('click', () => {
+  navMenu.style.right = '-30rem';
+  navMenu.style.visibility = 'hidden';
+  closeMenu.style.display = 'none';
+   openMenu.style.display = 'inline-block';
+});
